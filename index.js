5faca6883c7bd412b163ce83347e3c5d9c99af70
xella <xella.design@gmail.com>
2018-01-05 12:35:36 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.listen(3000, () => {
+  console.log('Server listening.')
+})
+
+app.get('/', (req, res, next) => {
+  res.render('index')
+})
